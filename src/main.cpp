467409a0027917a5c7d509e9157034cfa1483452
LooninS <hushed55@proton.me>
2026-06-17 10:08:06 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ int main() {
     }
     else if (input == "exit") break;
     else if (input.substr(0, 5) == "type ") {
-      std::string shit = " is a built-in command";
+      std::string shit = " is a builtin";
       if (input.substr(5) == "echo") {
         std::cout << "echo" << shit << std::endl;
       }
