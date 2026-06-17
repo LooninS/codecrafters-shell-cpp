33217177cb27fc67b62e3c688cbe769c9e2aa3ad
LooninS <hushed55@proton.me>
2026-06-17 10:38:29 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
+#include <filesystem>
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -7,6 +9,7 @@ int main() {
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
+  std::vector<std::string> args ={"echo", "exit", "type"};
   while (1) {
     std::cout << "$ ";
     std::string input;
@@ -16,22 +19,23 @@ int main() {
     }
     else if (input == "exit") break;
     else if (input.substr(0, 5) == "type ") {
-      std::string shit = " is a shell builtin";
-      if (input.substr(5) == "echo") {
-        std::cout << "echo" << shit << std::endl;
+      std::string command = input.substr(5);
+      for (auto &arg : args) {
+        if (arg == command) {
+          std::cout << command << " is a shell builtin" << std::endl;
+          break;
+        }
       }
-      else if (input.substr(5) == "exit") {
-        std::cout << "exit" << shit << std::endl;
+      std::string path = getenv("PATH");
+      std::filesystem::path p(path);
+      for (auto &entry : std::filesystem::directory_iterator(p)) {
+        if (entry.path().filename() == command) {
+          std::cout << input.substr(5) << " is " <<entry.path() << std::endl;
+          break;
+        }
       }
-      else if (input.substr(5) == "type") {
-        std::cout << "type" << shit << std::endl;
-      }
-      else std::cout << input.substr(5) << ": not found" << std::endl;
     }
-    else {
-      std::cout << input << ": command not found" << std::endl;
-    }
-
-
   }
 }
+
+
